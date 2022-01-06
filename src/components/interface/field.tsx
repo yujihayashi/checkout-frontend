b6c5c6b1671f74df07e8d2bacca8372fa102840d
Yuji Hayashi <yujihayashi87@gmail.com>
2022-01-05 23:25:37 -0300

    New structure

---
+++ src/components/interface/field.tsx
@@ -0,0 +1,8 @@
+export default function Field() {
+    return (
+    <div className="field__row">
+        <label htmlFor="name">Seu nome</label>
+        <input type="text" id="name" />
+    </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+      </main>
+
+    </div>
+  )
+}
